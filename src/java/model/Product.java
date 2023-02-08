db8dd10a9c4870372a14a1767258df60ec5d4e27
ba hung <hungnbhe160009@fpt.edu.vn>
2023-02-08 14:49:03 +0700

    first update

---
+++ src/java/model/Product.java
@@ -0,0 +1,84 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author BaHung
+ */
+public class Product {
+    private int productId,productPrice,categoryId;
+    private String productName,productColor,productDescription,image;
+
+    public Product() {
+    }
+
+    public Product(int productId, int productPrice, int categoryId, String productName, String productColor, String productDescription, String image) {
+        this.productId = productId;
+        this.productPrice = productPrice;
+        this.categoryId = categoryId;
+        this.productName = productName;
+        this.productColor = productColor;
+        this.productDescription = productDescription;
+        this.image = image;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(int productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductColor() {
+        return productColor;
+    }
+
+    public void setProductColor(String productColor) {
+        this.productColor = productColor;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+}
