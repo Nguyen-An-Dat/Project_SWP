cce7d323c44b0057d357df6b9833751e5ffcf017
HOANGHUNG\FPT SHOP <hunghmhe160003@fpt.edu.vn>
2023-03-01 14:11:00 +0700

    comment

--- build/web/assets/js/changeImage.js
+++
@@ -1,9 +0,0 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/JavaScript.js to edit this template
- */
-
- function change123123(src) {
-    document.getElementById("product-zoom").src = src;
-}
-
